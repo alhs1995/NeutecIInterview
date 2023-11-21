71bdb1e4976d9480c719c4ebf86ca8f80be5f106
alhs1995 <akimto.aki@gmail.com>
2023-11-21 23:19:44 +0800

    feat: 箱子閃爍,點點移動 鍵盤壞掉,用手機加減寫 目前耗時一小時

--- src/components/mainPage/answer1/box.vue
+++ src/components/mainPage/answer1/box.vue
@@ -1,17 +1,36 @@
 <script setup>
-
+const props = defineProps({
+  shine: {
+    type: Boolean,
+    withDefaults: false
+  }
+})
 </script>
 
 <template>
-  <div class="box"></div>
+  <div class="box" :class="{'shine':props.shine}"></div>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .box {
   flex: 0 0 calc(calc(100% - 6px) / 3);
   height: 100px;
   border: black solid 2px;
   background: radial-gradient(circle, rgba(113,81,95,1) 81%, rgba(0,0,0,1) 100%);
   box-sizing: border-box;
+  &.shine{
+    animation: shining 1s linear infinite;
+  }
+}
+@keyframes shining {
+  0%{
+    opacity: 1;
+  }
+  50%{
+    opacity: .6;
+  }
+  100%{
+    opacity: 1;
+  }
 }
 </style>
